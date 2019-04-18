24908e90056511ae7e892774d28f5a4f8e996383
Kevin Lorenzo <me@kevinlorenzo.com>
2019-04-18 01:53:08 -0600

    Second JUnit example added.

---
+++ src/test/java/com/kevinlorenzo/learning/junit/PasswordUtilTest.java
@@ -0,0 +1,30 @@
+package com.kevinlorenzo.learning.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.kevinlorenzo.learning.junit.PasswordUtil.SecurityLevel.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_characters() {
+        Assert.assertEquals(WEAK, PasswordUtil.assessPassword("1234567"));
+    }
+
+    @Test
+    public void weak_when_has_only_letters() {
+        Assert.assertEquals(WEAK, PasswordUtil.assessPassword("abcdef"));
+    }
+
+    @Test
+    public void medium_when_has_letters_and_numbers() {
+        Assert.assertEquals(MEDIUM, PasswordUtil.assessPassword("abcdef123"));
+    }
+
+    @Test
+    public void strong_when_has_letters_numbers_and_symbols() {
+        Assert.assertEquals(STRONG, PasswordUtil.assessPassword("abcdef123&"));
+    }
+
+}
