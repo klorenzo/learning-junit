a49f39c89ef14d8d30a8e45649107fe3b41ddf6c
Kevin Lorenzo <me@kevinlorenzo.com>
2019-04-20 15:14:52 -0600

    Fifth JUnit example added.

---
+++ src/test/java/com/kevinlorenzo/learning/junit/payments/PaymentProcessorBeforeTest.java
@@ -0,0 +1,39 @@
+package com.kevinlorenzo.learning.junit.payments;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class PaymentProcessorBeforeTest {
+
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
+    @Test
+    public void payment_is_correct() {
+
+        Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
+
+        Assert.assertTrue(paymentProcessor.makePayment(1000));
+
+    }
+
+    @Test
+    public void payment_is_wrong() {
+
+        Mockito.when(paymentGateway.requestPayment(Mockito.any()))
+                .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
+
+        Assert.assertFalse(paymentProcessor.makePayment(1000));
+
+    }
+
+}
